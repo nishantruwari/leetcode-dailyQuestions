16bed5d59d92e62519851418cfe6b20d2c881423
nishantruwari <83151342+nishantruwari@users.noreply.github.com>
2025-03-01 23:22:44 +0530

    Create lc2460.java

---
+++ lc2460.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        int n=nums.length;
+        for(int i=0;i<n-1;i++){
+            if(nums[i]==nums[i+1]){
+                nums[i]=nums[i]<<1;
+                nums[i+1]=0;
+                ++i;
+            }
+        }
+
+        int j=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                nums[j]=nums[i];
+                ++j;
+            }
+        }
+        for(;j<n;j++){
+            nums[j]=0;
+        }
+
+        return nums;
+
+        
+    }
+}
